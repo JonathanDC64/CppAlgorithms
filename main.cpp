ff86dd668d72dcbf2e1b2f3c8f87b3b9b0fac6b7
JonathanDC64 <nintendo_jon64@hotmail.com>
2021-10-02 17:49:17 -0400

    Implementation of selection sort (#1)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <vector>
+#include "sorting/selection-sort.h"
 
 using namespace std;
 
+void print_array(const vector<int> &array)
+{
+  for (int i : array)
+  {
+    cout << i << ",";
+  }
+  cout << endl;
+}
+
 int main()
 {
-  cout << "Hello World" << endl;
+  cout << "Selection sort: " << endl;
+  vector<int> selection_sort_values = {64, 25, 12, 22, 11};
+  selection_sort(&selection_sort_values[0], selection_sort_values.size());
+  print_array(selection_sort_values);
+  return 0;
 }
